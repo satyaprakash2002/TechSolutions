2406d71291f0809baa2fd3d9de638574d99aab1c
Satyaprakash Sarangi <sarangisatya2002@gmail.com>
2023-12-18 13:38:48 +0530

    main page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navigation from './SubComponents/Navigation'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
@@ -25,11 +26,8 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/SubComponents/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <>
+      <div className='nav'>
+        <p style={{fontWeight:'bold',fontSize:'1.2rem'}}>LOGO</p>
+        <div className="nav-items">
+          <p>Projects</p>
+          <p>About</p>
+          <p>Contact</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navigation
