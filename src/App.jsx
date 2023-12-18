ced696eb548d98bf51a9f108ef9c2a06b3715582
Satyaprakash Sarangi <sarangisatya2002@gmail.com>
2023-12-18 14:16:48 +0530

    phase 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navigation from './SubComponents/Navigation'
+import char from './images/char.png'
 
 function App() {
   
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h2> HI dv here 
-        
-      </h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="main-container">
+        <Navigation />
+        <div>
+          <img src={char} className='char'/>
+        </div>
       </div>
     </>
   );
