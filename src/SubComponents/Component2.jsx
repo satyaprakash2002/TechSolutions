820730486858556ae1974bb70ff328a488e3a2bb
Satyaprakash Sarangi <sarangisatya2002@gmail.com>
2024-01-03 11:30:58 +0530

    phae 5

--- src/SubComponents/Component2.jsx
+++ src/SubComponents/Component2.jsx
@@ -1,7 +1,7 @@
 // Component2.jsx
 
-import React from 'react';
-import img1 from '../images/techAiB.jpg'; // Update the file extension
+import React from "react";
+import img1 from "../images/techAiB.jpg"; // Update the file extension
 
 const Component2 = () => {
   return (
@@ -9,7 +9,9 @@ const Component2 = () => {
       <div className="content">
         <div className="text">
           {/* Write your content here */}
-          <p> Welcome to [Your Company Name], where innovation meets excellence!
+          <p>
+            {" "}
+            Welcome to [Your Company Name], where innovation meets excellence!
             As a premier development service agency, we bring your digital
             dreams to life. Whether you're seeking cutting-edge web development,
             seamless mobile app solutions, or transformative digital
@@ -20,16 +22,9 @@ const Component2 = () => {
             design, we are your trusted partner on the journey to digital
             success. Elevate your online presence with us – where creativity
             meets functionality, and your vision becomes our mission. Let's
-            build the future together!</p>
+            build the future together!
+          </p>
         </div>
-        <div className="imageSize">
-          {/* Insert your image source here */}
-          <img src={img1} alt="Description" />
-        </div>
-      </div>
-      <div className="carousel">
-        {/* Add your carousel component here */}
-        <p>This is the carousel at the bottom of the container.</p>
       </div>
     </div>
   );
